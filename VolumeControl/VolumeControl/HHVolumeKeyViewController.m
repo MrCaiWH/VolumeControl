bc92e4d35e5dd8fd22b70f1c322d232f7e7aa15c
huanghuacai <18811108252@163.com>
2016-05-07 19:42:02 +0800

    完成点击音量实体键功能

--- VolumeControl/VolumeControl/HHVolumeKeyViewController.m
+++ VolumeControl/VolumeControl/HHVolumeKeyViewController.m
@@ -13,6 +13,8 @@
 @interface HHVolumeKeyViewController ()
 /** 系统当前音量Label*/
 @property (weak, nonatomic) IBOutlet UILabel *volumeLabel;
+/** 另一种方法获取系统当前音量Label*/
+@property (weak, nonatomic) IBOutlet UILabel *otherVolumeLabel;
 /** 音量View*/
 @property (nonatomic, strong) MPVolumeView *volumeView;
 /** 设置音量滚动View*/
@@ -21,6 +23,7 @@
 
 @implementation HHVolumeKeyViewController
 
+#pragma mark - ViewLife
 - (void)viewDidLoad {
     [super viewDidLoad];
     
@@ -30,11 +33,21 @@
     
     [self p_addObserver];
     
-    //获取当前系统音量
-    float volume = [[AVAudioSession sharedInstance] outputVolume];
-    self.volumeLabel.text = [NSString stringWithFormat:@"%f",volume];
+    [self p_addObserverControlCenter];
+
+    [self p_getSystemVolume];
 }
 
+#pragma mark - IBActions
+- (IBAction)ChangeSystemVolumeClick:(UIButton *)sender {
+    //改变系统音量，这个值的范围为0.0f and 1.0f
+    [self.volumeViewSlider setValue:1.0f animated:NO];
+    
+    //    // send UI control event to make the change effect right now.
+    //    [self.volumeViewSlider sendActionsForControlEvents:UIControlEventTouchUpInside];
+}
+
+#pragma mark - Private
 /**
  *  获取音量监听视图
  */
@@ -67,6 +80,30 @@
 }
 
 /**
+ *  应用程序打开控制中心时，会走以下两个通知
+ */
+- (void)p_addObserverControlCenter {
+    //应用程序将要进入后台之前
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(willResignActive) name:UIApplicationWillResignActiveNotification object:nil];
+    //应用程序切回到前台
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didBecomeActive) name:UIApplicationDidBecomeActiveNotification object:nil];
+}
+
+/**
+ *  获取系统音量的两种办法
+ */
+- (void)p_getSystemVolume {
+    //方法一
+    float volume = [[AVAudioSession sharedInstance] outputVolume];
+    self.volumeLabel.text = [NSString stringWithFormat:@"%f",volume];
+    
+    //方法二
+    float systemVolume = self.volumeViewSlider.value;
+    self.otherVolumeLabel.text = [NSString stringWithFormat:@"%f",systemVolume];
+}
+
+#pragma mark - NSNotification Event
+/**
  *  点击音量键出发事件
  */
 - (void)volumeChanged:(NSNotification *)notification {
@@ -75,16 +112,21 @@
     NSDictionary *dic = [notification userInfo];
     self.volumeLabel.text = [NSString stringWithFormat:@"%@",dic[@"AVSystemController_AudioVolumeNotificationParameter"]];
     
+    self.otherVolumeLabel.text = [NSString stringWithFormat:@"%@",dic[@"AVSystemController_AudioVolumeNotificationParameter"]];
+}
 
-    
-    //    // retrieve system volume
-    //    //    float systemVolume = volumeViewSlider.value;
-    //
-    //    // change system volume, the value is between 0.0f and 1.0f
-    //    [volumeViewSlider setValue:1.0f animated:NO];
-    //
-    //    // send UI control event to make the change effect right now.
-    //    [volumeViewSlider sendActionsForControlEvents:UIControlEventTouchUpInside];
+/**
+ *  打开控制中心，对音量键按钮的控制失效
+ */
+- (void)willResignActive {
+     [[NSNotificationCenter defaultCenter] removeObserver:self name:@"AVSystemController_SystemVolumeDidChangeNotification" object:nil];
+}
+
+/**
+ *  退出控制中心，重新控制音量键按钮
+ */
+- (void)didBecomeActive {
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(volumeChanged:) name:@"AVSystemController_SystemVolumeDidChangeNotification" object:nil];
 }
 
 @end
